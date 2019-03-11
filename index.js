eefca419aea886d5edab1f9804b68985ada88607
Danny Tseng <dtfocus1025@gmail.com>
2019-03-11 05:11:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theNeatlesPlay(musicians, instruments) {
   var temp = [];
-  for (i =0; i < musicians.length; i++) {
-    
+  for (i = 0; i < musicians.length; i++) {
+    for (j )
   }
 }
